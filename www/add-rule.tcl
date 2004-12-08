5044f719ad5f3b73ee60834088fb9f49063d00db
Anny Flores <annyflores@viaro.net>
2004-12-08 21:23:33 +0000

    add changes

--- www/add-rule.tcl
+++ www/add-rule.tcl
@@ -15,26 +15,27 @@ ad_page_contract {
 set context [list "Add rule"]
 
 set assessments [rules::rule::get_assessments]
-form create add_rule 
-element create add_rule rule_name \
-     -datatype text\
-     -widget text\
-     -label "Rule Name"
-    
-element create add_rule asm_id\
-      -datatype text\
-      -widget select\
-      -label "Assessment Related"\
-      -options $assessments
+ad_form -name  add_rule  -form {
+     
+    { rule_name:text(text) 
+	{label "Rule Name"}
+    }
+    {asm_id:text(select)
+	{label "Assessment Related"}
+        {options $assessments}
+    }
+    {active_p:text(select)
+	{label "Active"}
+	{options {{Yes y} { No n}}}
+    }
+} -on_submit  {
+     set table_name "rules"
+     set id_column_name "rule_id"
+     set return_url "index"
+     set generated_id "rule_id"
 
-element create add_rule active_p\
-      -datatype text\
-      -widget select\
-      -label "Active"\
-      -options {{Yes y} { No n}}
+     rules::rule::new_rule  -rule_name $rule_name -asm_id $asm_id -active_p $active_p
     
-if {[template::form is_valid add_rule]} {
-    template::form get_values add_rule rule_name asm_id active_p
-    rules::rule::new_rule  -rule_name $rule_name -asm_id $asm_id -active_p $active_p 
-    ad_returnredirect "index"
+} -after_submit {
+  ad_returnredirect "index"
 }
